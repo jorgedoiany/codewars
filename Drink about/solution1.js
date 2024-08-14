9815e9a006bb8cec7083c4118d71bf45fa1ee4e9
Jorge Peguero <119319498+jorgedoiany@users.noreply.github.com>
2024-08-14 13:58:33 -0400

    Create solution1.js

---
+++ Drink about/solution1.js
@@ -0,0 +1,11 @@
+function peopleWithAgeDrink(age) {
+  if (age <= 13) {
+    return 'drink toddy';
+  } else if (age <= 17) {
+    return 'drink coke';
+  } else if (age <= 20) {
+    return 'drink beer';
+  } else {
+    return 'drink whisky';
+  }
+}
