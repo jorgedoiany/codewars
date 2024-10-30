42233c0ecd9bb3d0ddca19ee5e586fc67c9b7db8
Jorge Peguero <119319498+jorgedoiany@users.noreply.github.com>
2024-10-30 09:57:54 -0400

    Create solution1.js

---
+++ Categorize New Member/solution1.js
@@ -0,0 +1,12 @@
+function openOrSenior(data){
+  let category = [];
+  
+  for (let i = 0; i < data.length; i++) {
+    if (data [i][0] >= 55 && data [i][1] > 7) {
+      category.push('Senior')
+    } else {
+      category.push('Open')
+    }
+  }
+  return category;
+};
